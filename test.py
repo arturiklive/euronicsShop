9521f16af87ab3145bb332aa97a6231c13b9e34c
artur <tehnique@inbox.lv>
2023-01-23 23:12:31 +0200

    Adding code

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,21 +10,25 @@ from selenium.webdriver.support.wait import WebDriverWait
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-searchText = "apple m1"
-linkSearchField = "//input[@class='autocomplete__input']"
-linkSearchFieldGo = "//i[@class=' far fa-search']"
-linkSearchResult = "//h1"
-
+search_text = "apple m1"
 driver.get("https://www.euronics.lv/")
-driver.find_element("xpath", linkSearchField).send_keys(searchText)
-driver.find_element("xpath", linkSearchFieldGo).click()
 
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, linkSearchResult)))
-searchResultText = driver.find_element("xpath", linkSearchResult).text
-print(searchText)
-print(searchResultText)
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "cookie-accept-all-button")))
+if (driver.find_element(By.ID, "cookie-accept-all-button")):
+    driver.find_element(By.ID, "cookie-accept-all-button").click()
 
-assert searchResultText in searchText
+driver.find_element("xpath", "//input[@class='autocomplete__input']").send_keys(search_text)
+driver.find_element("xpath", "//i[@class=' far fa-search']").click()
+
+WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//h1[@class='category__header']")))
+
+search_result_text = driver.find_element("xpath", "//h1[@class='category__header']").text
+
+assert search_result_text in search_text
+
+random_product_number = random.randint(1,10)
+print(random_product_number)
+driver.find_element("xpath", "(//span[@class='product-card__title'])["+str(random_product_number)+"]").click()
 
 time.sleep(5)
 driver.close()
